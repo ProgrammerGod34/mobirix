5ff3a143b45c1ecb241a774f1d0ecc8a19c868e3
Александр Балабан <brooololo123@gmail.com>
2021-07-18 20:25:49 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import './App.css'
-import Home from './Component/Home/Home'
-import PagesTest from './Component/PagesTest/PagesTest'
-import { Route } from 'react-router-dom'
-import DownloadPage from './Component/DownloadPage/DownloadPage'
-import IconApp from './Component/IconApp/IconApp'
+import "./App.css";
+import Home from "./Component/Home/Home";
+import PagesTest from "./Component/PagesTest/PagesTest";
+import { Route } from "react-router-dom";
+import DownloadPage from "./Component/DownloadPage/DownloadPage";
+import IconApp from "./Component/IconApp/IconApp";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   return (
-    <div className='appContainer'>
+    <div className="appContainer">
       <IconApp />
-      <Route exact path='/' component={Home} />
-      <Route path='/home' component={Home} />
-      <Route path='/question' component={PagesTest} />
-      <Route path='/downloadpage' component={DownloadPage} />
+      <Route exact path="/">
+        {({ match }) => (
+          <CSSTransition
+            timeout={1000}
+            classNames="page"
+            unmountOnExit
+            in={match != null}
+          ><div className='page'><Home/></div></CSSTransition>
+        )}
+      </Route>
+      <Route path="/home">
+        {({ match }) => (
+          <CSSTransition
+            timeout={1000}
+            classNames="page"
+            unmountOnExit
+            in={match != null}
+          ><div className='page'><Home/></div></CSSTransition>
+        )}
+      </Route>
+      <Route path="/question">
+        {({ match }) => (
+          <CSSTransition
+            timeout={1000}
+            classNames="page"
+            unmountOnExit
+            in={match != null}
+          ><div className='page'><PagesTest/></div></CSSTransition>
+        )}
+      </Route>
+      <Route path="/downloadpage">
+        {({ match }) => (
+          <CSSTransition
+            timeout={1000}
+            classNames="page"
+            unmountOnExit
+            in={match != null}
+          ><div className='page'><DownloadPage/></div></CSSTransition>
+        )}
+      </Route>
     </div>
   );
 }
